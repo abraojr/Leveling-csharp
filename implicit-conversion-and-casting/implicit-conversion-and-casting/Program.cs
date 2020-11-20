6b9d7e73ef7c7168106d5036e74a3f05273f8f11
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-20 07:46:17 -0300

    Implicit Conversion and Casting

---
+++ implicit-conversion-and-casting/implicit-conversion-and-casting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace implicit_conversion_and_casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            float b;
+            a = 5.1;
+            b = (float)a;
+            Console.WriteLine(b); //OUTPUT: 5,1
+            Console.WriteLine();
+
+            double c;
+            int d;
+            c = 5.1;
+            d = (int)c;
+            Console.WriteLine(d); //OUTPUT: 5
+            Console.WriteLine();
+
+            int e = 5;
+            int f = 2;
+            double result = (double)e / f; //OUTPUT: 2,5
+            Console.WriteLine(result);
+        }
+    }
+}
