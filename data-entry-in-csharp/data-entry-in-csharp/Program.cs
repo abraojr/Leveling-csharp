9fde642fa65bda9da2d2d9926a0bdf21c4be1eda
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-20 08:17:57 -0300

    C# Data Entry Example 2

--- data-entry-in-csharp/data-entry-in-csharp/Program.cs
+++ data-entry-in-csharp/data-entry-in-csharp/Program.cs
@@ -1,30 +1,34 @@
 ﻿using System;
+using System.Globalization;
 
 namespace entrada_de_dados_no_csharp
 {
     class Program
     {
-        /*Make a program that: 1) Read a text until the line breaks and store it in a variable.
-         *2) Read three words, one on each line, storing each in a variable.
-         * 3) Read three words on the same line, separated by space, storing each one in a variable.*/
+        /*Make a program that: 1) Read an integer
+         * 2)Read a character
+         * 3)Read a double number.
+         * 4)Read a name (single word), gender (F or M character), age (integer) and height (double) on the same line,
+         * storing it in four variables with the appropriate types*/
         static void Main(string[] args)
         {
-            string phrase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
-            string[] v = Console.ReadLine().Split(' ');
-            string a = v[0];
-            string b = v[1];
-            string c = v[2];
-            Console.WriteLine("You typed: ");
-            Console.WriteLine(phrase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            string[] vet = Console.ReadLine().Split(' ');
+            string name = vet[0];
+            char gender = char.Parse(vet[1]);
+            int age = int.Parse(vet[2]);
+            double height = double.Parse(vet[3], CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("You typed:");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(name);
+            Console.WriteLine(gender);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
