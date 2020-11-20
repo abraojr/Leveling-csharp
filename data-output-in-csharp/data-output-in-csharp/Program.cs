7dc4ec03989afe21ea9ac5f7ab86b04289714ab9
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-20 07:10:53 -0300

    Using System.Globalization to format doubles

---
+++ data-output-in-csharp/data-output-in-csharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace data_output_in_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char gender = 'M';
+            string name = "Abrão";
+            double balance = 10.56785;
+
+            Console.WriteLine(gender);
+            Console.WriteLine(name);
+            Console.WriteLine(balance);
+            Console.WriteLine(balance.ToString("F2"));
+            Console.WriteLine(balance.ToString("F4"));
+            Console.WriteLine(balance.ToString("F4", CultureInfo.InvariantCulture));
+
+            /*OUTPUT:
+            M
+            Abrão
+            10,56785
+            10,57
+            10,5678
+            10.5678 */
+        }
+    }
+}
