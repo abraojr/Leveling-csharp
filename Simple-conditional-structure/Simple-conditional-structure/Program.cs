f021084fa1d4d33a4e8a4052d7da198986679c15
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-30 02:10:53 -0300

    Demo: Simple conditional structure

---
+++ Simple-conditional-structure/Simple-conditional-structure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Simple_conditional_structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            Console.WriteLine("Good morning");
+
+            if (x < 5)
+            {
+                Console.WriteLine("Good afternoon");
+            }
+            Console.WriteLine("Good night");
+        }
+    }
+}
