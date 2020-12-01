604f1999b5229407c0cfc9e5176a43f36ae8c596
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-30 21:48:19 -0300

    Demo of functions: first example without any function

---
+++ functions/functions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program to read three integers numbers and show the largest one on the screen.*/
+
+            Console.WriteLine("Enter three integers numbers: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("The largest number is: " + n1);
+            }
+            else if (n2 > n3)
+            {
+                Console.WriteLine("The largest number is: " + n2);
+            }
+            else
+            {
+                Console.WriteLine("The largest number is: " + n3);
+            }
+        }
+    }
+}
