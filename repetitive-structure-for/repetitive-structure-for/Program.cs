ee598b9ea93231ce1570b361c1eb70b741859e2a
Abrão Astério Junior <abcorinthians@gmail.com>
2020-11-30 22:23:03 -0300

    Demo of the repetitive structure 'for'

---
+++ repetitive-structure-for/repetitive-structure-for/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace repetitive_structure_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Enter an N number and then N integer values. Show the sum of the N values entered.*/
+
+
+            Console.Write("How many integer numbers are you going to type? ");
+            int N = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write("Value #{0}: ", i);
+                int value = int.Parse(Console.ReadLine());
+                sum += value;
+            }
+            Console.WriteLine("Sum = " + sum);
+        }
+    }
+}
